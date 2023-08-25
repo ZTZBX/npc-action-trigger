dd6f698ea3079544d38e5c7be13b1dd3b4b59a20
zabbix-byte <zabbix@ztrunk.space>
2023-08-25 10:46:52 +0200

    base

---
+++ Client/ClientMain.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace npc_action_trigger.Client
+{
+    public class ClientMain : BaseScript
+    {
+        public ClientMain()
+        {
+        }
+    }
+}
---
+++ Server/ServerMain.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace npc_action_trigger.Server
+{
+    public class ServerMain : BaseScript
+    {
+        public ServerMain()
+        {
+        }
+    }
+}
